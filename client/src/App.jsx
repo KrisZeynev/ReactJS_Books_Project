60d4891d1f0e1a37c5ea8065ae1c660a6b463815
Kris Zeynev <122886072+KrisZeynev@users.noreply.github.com>
2025-03-20 23:59:16 +0200

    Update Home.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,10 +12,10 @@ function App() {
       <div className="min-h-screen flex flex-col">
         <Header/>
         <main className="flex-grow">
-          {/* <Home></Home> */}
           {/* <NotFoundPage/> */}
           {/* <Login/> */}
-          <Register/>
+          {/* <Register/> */}
+          <Home/>
         </main>
         <Footer />
       </div>
