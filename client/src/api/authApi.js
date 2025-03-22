14c190f40d9ca7d4c3025564670083f521c1a227
Kris Zeynev <122886072+KrisZeynev@users.noreply.github.com>
2025-03-22 20:49:56 +0200

    Add useLogout

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useContext } from "react";
 import request from "../utils/request";
+import { UserContext } from "../contexts/UserContext";
 
 
 const baseUrl = 'http://localhost:3030/users';
@@ -25,4 +26,28 @@ export const useLogin = () => {
     return {
         login,
     }
+};
+
+export const useLogout = () => {
+    const { accessToken, userLogoutHandler } = useContext(UserContext);
+
+    useEffect(() => {
+        if (!accessToken) {
+            return;
+        }
+
+        const options = {
+            headers: {
+                'X-Authorization': accessToken,
+            }
+        };
+
+        request.get(`${baseUrl}/logout`, null, options)
+            .then(userLogoutHandler);
+
+    }, [accessToken, userLogoutHandler]);
+
+    return {
+        isLoggedOut: !!accessToken,
+    };
 };
