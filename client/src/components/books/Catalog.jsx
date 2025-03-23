b86da58c7b878f5d6a575c82e4d83b9f8980c582
Kris Zeynev <122886072+KrisZeynev@users.noreply.github.com>
2025-03-23 18:54:05 +0200

    Add dummy data to check the current representation of the catalog page

--- client/src/components/books/Catalog.jsx
+++ client/src/components/books/Catalog.jsx
@@ -1,10 +1,84 @@
+import BookDetailsCard from "../book-details/BookDetailsCard";
 import SearchBar from "../search-bar/SearchBar";
 
-export default function Catalog () {
-    return (
-        <>
-        <h1>Catalog</h1>
-        <SearchBar/>
-        </>
-    )
+export default function Catalog() {
+  const featuredBooks = [
+    {
+      id: 1,
+      title: "The Great Gatsby",
+      author: "F. Scott Fitzgerald",
+      cover:
+        "https://media.istockphoto.com/id/949118068/photo/books.jpg?s=612x612&w=0&k=20&c=1vbRHaA_aOl9tLIy6P2UANqQ27KQ_gSF-BH0sUjQ730=",
+    },
+    {
+      id: 2,
+      title: "To Kill a Mockingbird",
+      author: "Harper Lee",
+      cover:
+        "https://t4.ftcdn.net/jpg/09/42/21/27/360_F_942212780_zdL34kBycfWYH1rOzzTivvuvJCGAg20O.jpg",
+    },
+    {
+      id: 3,
+      title: "1984",
+      author: "George Orwell",
+      cover:
+        "https://static.vecteezy.com/system/resources/thumbnails/034/039/987/small/man-in-a-tunnel-made-of-books-books-and-knowledge-concept-generated-by-artificial-intelligence-photo.jpg",
+    },
+    {
+      id: 4,
+      title: "Moby Dick",
+      author: "Herman Melville",
+      cover: "https://images.pexels.com/photos/694740/pexels-photo-694740.jpeg",
+    },
+    {
+      id: 5,
+      title: "Pride and Prejudice",
+      author: "Jane Austen",
+      cover: "https://images.pexels.com/photos/46274/pexels-photo-46274.jpeg",
+    },
+    {
+      id: 6,
+      title: "The Catcher in the Rye",
+      author: "J.D. Salinger",
+      cover: "https://images.pexels.com/photos/590493/pexels-photo-590493.jpeg",
+    },
+    {
+      id: 7,
+      title: "Brave New World",
+      author: "Aldous Huxley",
+      cover: "https://images.pexels.com/photos/256455/pexels-photo-256455.jpeg",
+    },
+    {
+      id: 8,
+      title: "Crime and Punishment",
+      author: "Fyodor Dostoevsky",
+      cover:
+        "https://images.pexels.com/photos/3747468/pexels-photo-3747468.jpeg",
+    },
+    {
+      id: 9,
+      title: "The Hobbit",
+      author: "J.R.R. Tolkien",
+      cover: "https://images.pexels.com/photos/356372/pexels-photo-356372.jpeg",
+    },
+    {
+      id: 10,
+      title: "War and Peace",
+      author: "Leo Tolstoy",
+      cover:
+        "https://images.pexels.com/photos/159866/books-book-pages-read-literature-159866.jpeg",
+    },
+  ];
+
+  return (
+    <>
+      <h1>Catalog</h1>
+      <SearchBar />
+      <div className="grid grid-cols-1 md:grid-cols-5 gap-20">
+        {featuredBooks.map((book) => (
+          <BookDetailsCard key={book.id} book={book} />
+        ))}
+      </div>
+    </>
+  );
 }
