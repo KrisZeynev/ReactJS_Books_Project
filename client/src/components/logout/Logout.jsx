8c27df0bc425c567e06de536c5d85863501872be
Kris Zeynev <122886072+KrisZeynev@users.noreply.github.com>
2025-03-23 22:59:44 +0200

    Adjust the spinner

--- client/src/components/logout/Logout.jsx
+++ client/src/components/logout/Logout.jsx
@@ -1,20 +1,22 @@
 import { Navigate } from "react-router";
-import { useLogout } from "../../api/authApi"
+import { useLogout } from "../../api/authApi";
 
 export default function Logout() {
-    const {isLoggedOut} = useLogout()
-    return (
-        <>
-        {/* {isLoggedOut} ? <Navigate to='/login'/> ? <Spinner/> */}
-        {isLoggedOut} ? <Navigate to='/login'/> ? <Spinner/>
-        </>
-    )
+  const { isLoggedOut } = useLogout();
+  console.log(isLoggedOut);
+  
+  return (
+    <>
+      {/* {isLoggedOut} ? <Navigate to='/login'/> ? <Spinner/> */}
+      {isLoggedOut} ? <Navigate to="/login" /> ? <Spinner />
+    </>
+  );
 }
 
-function Spinner () {
-    return (
-      <div className="flex items-center justify-center">
-        <div className="w-8 h-8 border-4 border-gray-300 border-t-indigo-500 rounded-full animate-spin"></div>
-      </div>
-    );
-  };
+function Spinner() {
+  return (
+    <div className="flex items-center justify-center mt-50">
+      <div className="w-16 h-16 border-4 border-gray-300 border-t-indigo-500 rounded-full animate-spin"></div>
+    </div>
+  );
+}
