cdcb112d8be100beda5dc3a3c2432f6408fe3c82
Kris Zeynev <122886072+KrisZeynev@users.noreply.github.com>
2025-03-23 18:47:34 +0200

    Add the search bar into the catalog page

--- client/src/components/books/Catalog.jsx
+++ client/src/components/books/Catalog.jsx
@@ -1,5 +1,10 @@
+import SearchBar from "../search-bar/SearchBar";
+
 export default function Catalog () {
     return (
+        <>
         <h1>Catalog</h1>
+        <SearchBar/>
+        </>
     )
 }
