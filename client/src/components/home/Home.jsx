2c5e55631fd2a9f2e016bfbcf2cd617ab0a7f0b0
Kris Zeynev <kriszeynev@yahoo.com>
2025-04-02 19:08:36 +0300

    Fix home page

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -20,35 +20,40 @@ export default function Home() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center text-center p-6">
-      <h1 className="text-4xl font-bold text-gray-800 mb-4">
-        Welcome to ReactJS Books Project 📚
-      </h1>
-      <p className="text-lg text-gray-600 mb-6">
-        Discover, rate, and comment your favorite books!
-      </p>
+    <>
+      {featuredBooks.length > 0 ? (
+        <div className="flex flex-col items-center justify-center text-center p-6">
+          <h1 className="text-4xl font-bold text-gray-800 mb-4">
+            Welcome to ReactJS Books Project 📚
+          </h1>
+          <p className="text-lg text-gray-600 mb-6">
+            Discover, rate, and comment your favorite books!
+          </p>
 
-      <h2 className="text-2xl font-semibold mb-4">Featured Books</h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 gap-x-25">
-        {featuredBooks.length > 0 ? (
-          featuredBooks.map((book) => (
-            <BookDetailsCard
-              key={book._id}
-              book={book}
-              handleDelete={handleDelete}
-            />
-          ))
-        ) : (
-          <h1 className="text-2xl font-semibold text-gray-500 mt-6 bg-red-100 px-6 py-3 rounded-lg border border-red-400 shadow-md">
+          <h2 className="text-2xl font-semibold mb-4">Featured Books</h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6 gap-x-25">
+            {featuredBooks.map((book) => (
+              <BookDetailsCard
+                key={book._id}
+                book={book}
+                handleDelete={handleDelete}
+              />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className="flex flex-col items-center justify-center text-center p-6">
+          <h1 className="text-4xl font-bold text-gray-800 mb-4">
+            Welcome to ReactJS Books Project 📚
+          </h1>
+          <p className="text-lg text-gray-600 mb-6">
+            Discover, rate, and comment your favorite books!
+          </p>
+          <h1 className="text-2xl font-semibold text-gray-500 bg-red-100 px-6 py-3 rounded-lg border border-red-400 shadow-md">
             No books found!
           </h1>
-        )}
-      </div>
-      {/* {featuredBooks.length === 0 && (
-        <h1 className="text-2xl font-semibold text-gray-500 mt-6 bg-red-100 px-6 py-3 rounded-lg border border-red-400 shadow-md">
-          No books found!
-        </h1>
-      )} */}
-    </div>
+        </div>
+      )}
+    </>
   );
 }
