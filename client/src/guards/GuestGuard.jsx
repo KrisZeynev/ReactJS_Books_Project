beee31b1fa0f8c0a7432250f09c2381644a78eb5
Kris Zeynev <kriszeynev@yahoo.com>
2025-04-08 00:21:36 +0300

    Update the route guards

--- client/src/guards/GuestGuard.jsx
+++ client/src/guards/GuestGuard.jsx
@@ -2,8 +2,11 @@ import { useContext } from "react";
 import { Navigate, Outlet } from "react-router";
 import { UserContext } from "../contexts/UserContext";
 
+import { useSelector } from "react-redux";
+
 export default function GuestGuard() {
-  const { email } = useContext(UserContext);
+  // const { email } = useContext(UserContext);
+  const email = useSelector((state) => state.auth.email);
 
   if (email) {
     return <Navigate to="/" />;
